a9586fe8c3be2c4a99155b7076b8e5f2e2f4a54e
kvakili813 Vakili <kvakili813@gmail.com>
2017-09-29 00:53:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 def fizzbuzz(int)
-  if int % 3
+  if int % 3 == 0
     "Fizz"
 end
-if int % 5
+if int % 5 == 0
   "Buzz"
 end
-if int % 3 and 5
+if int % 3 and 5 == 0
   "FizzBuzz"
 end
-if int !% 3 || 5
+if int !% 3 || 5 == 0
   "nil"
 end
 end
